baff1e710720a6e40d547f20710d8be52dfaaa3e
herypank <60302361+herypank@users.noreply.github.com>
2020-08-22 15:09:56 +0300

    Добавил вывод комментариев

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import requests
 id_comic = 614
 url = f'https://xkcd.com/{id_comic}/info.0.json'
 response = requests.get(url)
-url_comic = (response.json())['img']
+decoded_response = response.json()
+url_comic = decoded_response['img']
+print(decoded_response['alt'])
 response = requests.get(url_comic)
 filename = f'comic{id_comic}.jpg'
 with open(filename, 'wb') as file:
