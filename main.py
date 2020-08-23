d0e40ffd8997ea26afd2af21a771dcf71672351b
herypank <60302361+herypank@users.noreply.github.com>
2020-08-23 22:35:05 +0300

    Добавил вывод групп

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+import os
 import requests
 from dotenv import load_dotenv
 
 
 chat_id = os.getenv('CHAT_ID')
+access_token = os.getenv('ACCESS_TOKEN')
 id_comic = 614
 url = f'https://xkcd.com/{id_comic}/info.0.json'
 response = requests.get(url)
 decoded_response = response.json()
 url_comic = decoded_response['img']
-print(decoded_response['alt'])
 response = requests.get(url_comic)
 filename = f'comic{id_comic}.jpg'
 with open(filename, 'wb') as file:
     file.write(response.content)
+url_get = f'https://api.vk.com/method/groups.get?access_token={access_token}&v=5.122'
+response = requests.get(url_get)
+print(response.text)
