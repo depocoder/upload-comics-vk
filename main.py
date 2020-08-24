0b92783776089cb05747d5812cd578a2f2d521f5
herypank <60302361+herypank@users.noreply.github.com>
2020-08-24 17:02:54 +0300

    Исправил ошибку в именовании

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 from dotenv import load_dotenv
 
 
-def gowload_comic(id_comic):
+def download_comic(id_comic):
     url = f'https://xkcd.com/{id_comic}/info.0.json'
     response = requests.get(url)
     decoded_response = response.json()
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     chat_id = os.getenv('CHAT_ID')
     access_token = os.getenv('ACCESS_TOKEN')
     group_id = os.getenv('GROUP_ID')
-    gowload_comic(614)
+    download_comic(614)
     server, photo, hash = upload_photo_on_server(group_id, access_token)
     url_get = f'https://api.vk.com/method/photos.saveWallPhoto?group_id=198114184&access_token={access_token}&v=5.122'
     response_uploadd = requests.post(url_get, params={
