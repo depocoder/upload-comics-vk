8f11cc8a1b8edd657369709dfedcedc6f22ffe73
herypank <60302361+herypank@users.noreply.github.com>
2020-08-24 15:19:38 +0300

    Добавил загрузку на сервер вк, рефакторинг кода

--- main.py
+++ main.py
@@ -3,17 +3,41 @@ import requests
 from dotenv import load_dotenv
 
 
-chat_id = os.getenv('CHAT_ID')
-access_token = os.getenv('ACCESS_TOKEN')
-id_comic = 614
-url = f'https://xkcd.com/{id_comic}/info.0.json'
-response = requests.get(url)
-decoded_response = response.json()
-url_comic = decoded_response['img']
-response = requests.get(url_comic)
-filename = f'comic{id_comic}.jpg'
-with open(filename, 'wb') as file:
-    file.write(response.content)
-url_get = f'https://api.vk.com/method/groups.get?access_token={access_token}&v=5.122'
-response = requests.get(url_get)
-print(response.text)
+def gowload_comic(id_comic):
+    url = f'https://xkcd.com/{id_comic}/info.0.json'
+    response = requests.get(url)
+    decoded_response = response.json()
+    url_comic = decoded_response['img']
+    response = requests.get(url_comic)
+    filename = f'comic{id_comic}.jpg'
+    with open(filename, 'wb') as file:
+        return file.write(response.content)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    chat_id = os.getenv('CHAT_ID')
+    access_token = os.getenv('ACCESS_TOKEN')
+    group_id = os.getenv('GROUP_ID')
+    gowload_comic(id_comic)
+    url_get = f'https://api.vk.com/method/photos.getWallUploadServer?group_id={group_id}&access_token={access_token}&v=5.122'
+    response = requests.get(url_get)
+    with open("comic614.jpg", 'rb') as file:
+        upload_url = response.json()['response']['upload_url']
+        files = {
+            'photo': file,
+        }
+        response_upload = requests.post(upload_url, files=files)
+        response_upload.raise_for_status()
+    decoded_response_upload = response_upload.json()
+    server = decoded_response_upload['server']
+    photo = decoded_response_upload['photo']
+    hash = decoded_response_upload['hash']
+    print(hash)
+    url_get = f'https://api.vk.com/method/photos.saveWallPhoto?group_id=198114184&access_token={access_token}&v=5.122'
+    response_uploadd = requests.post(url_get, params={
+        "server": server,
+        'photo': photo,
+        'hash': hash
+    })
+    print(response_uploadd.json())
