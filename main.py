b4524ca1e30d67ee11b8e98375e28d10a20bcf6c
herypank <60302361+herypank@users.noreply.github.com>
2020-08-24 17:51:15 +0300

    Добавил переменную name_comic, исправил нейминг

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ def download_comic(id_comic):
     response = requests.get(url)
     decoded_response = response.json()
     url_comic = decoded_response['img']
-    response = requests.get(url_comic)
+    download_response = requests.get(url_comic)
     filename = f'comic{id_comic}.jpg'
     with open(filename, 'wb') as file:
-        file.write(response.content)
+        file.write(download_response.content)
     return decoded_response['safe_title']
 
 
@@ -19,6 +19,7 @@ def upload_photo_on_server(group_id, access_token):
     url_get = ('https://api.vk.com/method/photos.getWallUploadServer?')
     response = requests.get(url_get, params={
         "group_id": group_id,
+        'album_id': "272963239",
         "access_token": access_token,
         'v': '5.122'
     })
@@ -41,7 +42,7 @@ if __name__ == "__main__":
     chat_id = os.getenv('CHAT_ID')
     access_token = os.getenv('ACCESS_TOKEN')
     group_id = os.getenv('GROUP_ID')
-    print(download_comic(614))
+    name_comic = download_comic(614)
     server, photo, hash = upload_photo_on_server(group_id, access_token)
     url_get = f'https://api.vk.com/method/photos.saveWallPhoto?group_id=198114184&access_token={access_token}&v=5.122'
     response_uploadd = requests.post(url_get, params={
@@ -52,4 +53,4 @@ if __name__ == "__main__":
     id_pic = response_uploadd.json()['response'][0]['id']
     owner_id = response_uploadd.json()['response'][0]['owner_id']
     url_get = f'https://api.vk.com/method/wall.post?attachments=photo{owner_id}_{id_pic}&access_token={access_token}&v=5.122'
-    print(requests.post(url_get).text)
+    #print(requests.post(url_get).text)
