8878ab3fb5b0959ab4f96f3a36e02770988af586
herypank <60302361+herypank@users.noreply.github.com>
2020-08-25 14:31:26 +0300

    Добавил вывод названия комикса , разбил функцию download_comic на две.

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ import requests
 from dotenv import load_dotenv
 
 
-def download_comic(comic_id):
+def get_comic_info(comic_id):
     url = f'https://xkcd.com/{comic_id}/info.0.json'
     response = requests.get(url)
-    decoded_response = response.json()
-    url_comic = decoded_response['img']
+    return response.json()
+
+
+def download_comic(comic_id):
+    url_comic = get_comic_info(comic_id)['img']
     download_response = requests.get(url_comic)
     filename = f'comic{comic_id}.jpg'
     with open(filename, 'wb') as file:
-        file.write(download_response.content)
-    return decoded_response['safe_title']
+        return file.write(download_response.content)
 
 
 def get_upload_url(group_id, vk_token):
@@ -44,7 +46,7 @@ if __name__ == "__main__":
     vk_token = os.getenv('VK_TOKEN')
     group_id = os.getenv('GROUP_ID')
     comic_id = 615
-    name_comic = download_comic(comic_id)
+    comic_name = get_comic_info(comic_id)['safe_title']
     upload_url = get_upload_url(group_id, vk_token)
     server, photo, hash = upload_photo_on_server(group_id, vk_token, upload_url, comic_id)
     url_get = f'https://api.vk.com/method/photos.saveWallPhoto?'
@@ -62,6 +64,7 @@ if __name__ == "__main__":
     response = requests.post(url_get, params={
         "attachments": f"photo{owner_id}_{id_pic}",
         'owner_id': f'-{group_id}',
+        "message": comic_name,
         "access_token": vk_token,
         'v': '5.122'
 
